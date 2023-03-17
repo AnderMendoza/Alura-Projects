b70df4ba50f98dba71e08124b2bc686e0f30b5d9
Anderson Mendoza <davidmramos53@gmail.com>
2023-03-16 19:30:56 -0500

    Create 03-BuenasPracticas.js

---
+++ projectPracticeJavaScript/03-BuenasPracticas.js
@@ -0,0 +1,7 @@
+let nombreEmpleado = "Jose Perez";
+let NombreEmpleado = "Pedro Ramirez";
+
+let edadEmpleado = 40;
+console.log(edadEmpleado);
+edadEmpleado = "Cuarenta";
+console.log(edadEmpleado);
