daeca1d45101882a50a7cdf5721f501fe08e7010
Anderson Mendoza <davidmramos53@gmail.com>
2023-03-29 21:41:28 -0500

    adding event in checkbox

--- projectDomWithJS/script.js
+++ projectDomWithJS/script.js
@@ -34,5 +34,13 @@ btn.addEventListener("click", createTask);
 const checkComplete = () => {
     const i = document.createElement("i");
     i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
     return i
+}
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
 }
