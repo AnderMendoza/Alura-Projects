8f8ed0f6540ef96308525a12bf6752608b42be20
Anderson Mendoza <davidmramos53@gmail.com>
2023-03-29 22:52:18 -0500

    exporting and importing js components

--- projectDomWithJS/script.js
+++ projectDomWithJS/script.js
@@ -1,46 +1,31 @@
-(() => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-    const btn = document.querySelector("[data-form-btn]");
+const btn = document.querySelector("[data-form-btn]");
 
-    //arrow functions o funciones anonimas
-    const createTask = ((evento) => {
-        evento.preventDefault();
-        const input = document.querySelector("[data-form-input]");
-        const value = input.value;
-        const list = document.querySelector("[data-list]");
-        const task = document.createElement("li");
-        task.classList.add("card");
-        input.value = "";
-        //backlist
-        const taskContent = document.createElement("div");
-        taskContent.appendChild(checkComplete());
-        const titleTask = document.createElement("span");
-        titleTask.classList.add("task");
-        titleTask.innerHTML = value;
-        taskContent.appendChild(titleTask);
-        const content = `
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
-        //task.innerHTML = content;
-        task.appendChild(taskContent);
+//arrow functions o funciones anonimas
+const createTask = ((evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+    //backlist
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    //task.innerHTML = content;
 
-        list.appendChild(task);
-    })
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
+    list.appendChild(task);
+})
 
-    //listener de un evento
-    //insertamos la referencia de createTask
-    btn.addEventListener("click", createTask);
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i
-    }
-
-    // Inmediately invoked function expression IIFE
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle("fas");
-        element.classList.toggle("completeIcon");
-        element.classList.toggle("far");
-    }
-})();
+//listener de un evento
+//insertamos la referencia de createTask
+btn.addEventListener("click", createTask);
